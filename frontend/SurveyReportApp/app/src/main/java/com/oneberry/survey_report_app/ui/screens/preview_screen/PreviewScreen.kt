c667c5370839a8cbd65a63591b5e05d60a0717d3
ShermanOneberry <107899491+ShermanOneberry@users.noreply.github.com>
2023-05-25 14:41:58 +0800

    Add additional viewmodel code

--- frontend/SurveyReportApp/app/src/main/java/com/oneberry/survey_report_app/ui/screens/preview_screen/PreviewScreen.kt
+++ frontend/SurveyReportApp/app/src/main/java/com/oneberry/survey_report_app/ui/screens/preview_screen/PreviewScreen.kt
@@ -1,5 +1,6 @@
 package com.oneberry.survey_report_app.ui.screens.preview_screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,6 +25,17 @@ fun PreviewScreen(
 ) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
     val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        previewViewModel
+            .toastMessage
+            .collect { message ->
+                Toast.makeText(
+                    context,
+                    message,
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+    }
     Column(
         modifier = Modifier
             .padding(contentPadding) //Margin
--- frontend/SurveyReportApp/app/src/main/java/com/oneberry/survey_report_app/ui/screens/preview_screen/PreviewViewModel.kt
+++ frontend/SurveyReportApp/app/src/main/java/com/oneberry/survey_report_app/ui/screens/preview_screen/PreviewViewModel.kt
@@ -2,15 +2,24 @@ package com.oneberry.survey_report_app.ui.screens.preview_screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.oneberry.survey_report_app.SurveyApplication
+import com.oneberry.survey_report_app.data.SurveyReportRepository
 import com.oneberry.survey_report_app.data.UserCredentialsRepository
 import com.oneberry.survey_report_app.network.PocketBaseRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class PreviewViewModel (
     private val userCredentialsRepository: UserCredentialsRepository,
     private val backendAPI: PocketBaseRepository,
+    private val surveyReportRepository: SurveyReportRepository
 ): ViewModel() {
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
@@ -21,12 +30,65 @@ class PreviewViewModel (
                     app.userCredentialsRepository
                 val backendAPI =
                     app.backendAPI
+                val surveyReportRepository =
+                    app.surveyReportRepository
                 PreviewViewModel(
                     userCredentialsRepository = userCredentialsRepository,
                     backendAPI = backendAPI,
+                    surveyReportRepository = surveyReportRepository,
                 )
             }
         }
     }
+    //Survey data
+    val surveyState = surveyReportRepository.mutableSurveyState.asStateFlow()
+    //Credentials data
+    private val credentialFlow = userCredentialsRepository.credentialsFlow
+    //Toast emitter
+    private val _toastMessage = MutableSharedFlow<String>()
+    val toastMessage = _toastMessage.asSharedFlow()
+    //Hooks
     //TODO: Add code here
+    fun triggerSubmission() {
+        //TODO: Adjust the toast messages to be more concerned with errors
+        // suddenly popping up when they shouldn't
+        viewModelScope.launch {
+            val finalFormData =
+                surveyState.value.copy(submissionTime = LocalDateTime.now())
+            if (!finalFormData.overallSurveyValid()) {
+                _toastMessage.emit("Current form is not valid")
+                return@launch
+            }
+            val credentials = credentialFlow.first() //TODO: Check this with multiple user changes
+            if (credentials.username == null) {
+                _toastMessage.emit("You need to login to submit")
+                return@launch
+            }
+            val nonNullCredentials =
+                credentials.tryGetNotNullCredentials()
+            if (nonNullCredentials == null) {
+                _toastMessage.emit("Please Login Again")
+                return@launch
+            }
+            if (nonNullCredentials.isNotExpired(LocalDateTime.now(),false)) {
+                _toastMessage.emit("You need to login again, your session has expired.")
+                //TODO: Trigger relogin dialog
+                return@launch
+            }
+            val surveyRequestID = "b7z7sachnw3uqlg"
+            val surveyResponseID = backendAPI.uploadForm(
+                nonNullCredentials.token,
+                surveyRequestID,
+                nonNullCredentials.id,
+                finalFormData
+            )
+            if (surveyResponseID == null) {
+                _toastMessage.emit("Unable to submit form")
+            } else {
+                _toastMessage.emit("Submission successful (ID: $surveyRequestID)")
+                surveyReportRepository.resetSurvey()
+                //TODO: Trigger navigation back to form
+            }
+        }
+    }
 }
