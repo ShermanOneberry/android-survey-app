83e56d2233fefe1aca7fa401b2ae03c12a1e832b
ShermanOneberry <107899491+ShermanOneberry@users.noreply.github.com>
2023-05-31 17:30:09 +0800

    Check in work done
    Partial work done on actual report generation script
    Minor work done on type annotation, but not ready for use yet

--- backend/pocketbase_scripts/generate_report.js
+++ backend/pocketbase_scripts/generate_report.js
@@ -1,20 +1,90 @@
-const PocketBase = require('pocketbase/cjs')
-var xl = require('excel4node');
+const PocketBase = require('pocketbase/cjs');
+const Excel = require('@siema-team/spreadsheets');
+const axios = require('axios');
 require('dotenv').config()
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
+var fileToken = null
+
+function axios_get_image_buffer(url) {
+    return axios
+    .get(url, {
+      responseType: 'arraybuffer'
+    })
+    .then(response => Buffer.from(response.data, 'binary'))
+    .catch(function (_) {
+        return null
+      });
+}
+
+async function get_image_from_pocketbase(record, imageRef) {
+    if (fileToken === null){
+        fileToken = await pb.files.getToken()
+    }
+    let url = pb.files.getUrl(record, imageRef, {'token': fileToken});
+    let buffer = await axios_get_image_buffer(url)
+    if (buffer !== null) { 
+        return buffer
+    }
+    fileToken = await pb.files.getToken()
+    url = pb.files.getUrl(record, imageRef, {'token': fileToken});
+    buffer = await axios_get_image_buffer(url)
+    if (buffer === null) { 
+        return null //TODO: Throw error
+    }
+    return buffer
+    //fileToken has expired, refresh it
+    //TODO: Get image and return if successful
+    //TODO: If not successful, get fresh file token and try agian
+    //TODO: If fail again, throw error
+    
+}
+
 async function generate_batch_report(batch_num, fileToken) {
-    const records = await pb.collection('surveyResults').getFullList({
-        expand: "surveyRequest",
+    const records = await pb.collection("surveyResults").getFullList({
+        expand: "surveyRequest,assignedUser",
         filter: `surveyRequest.batchNumber=${batch_num}`,
     });
-    var wb = new xl.Workbook();
-    var ws = wb.addWorksheet("Contractor's Deployment Plans");
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile("./template/Contractor Deployment Plan Batch XXX.xlsx");
+    const worksheet = workbook.getWorksheet("Contractor's Deployment Plans");
+    if (worksheet === undefined) {
+        return //TODO: Throw error
+    }
+    worksheet.getCell("A2").value = `BATCH NO: ${batch_num}`
     
-    records.array.forEach(record => {
+    records.forEach(record => {
+        console.log(record)
+        const rowOffset = 5
+        const originalRequest = record.expand.surveyRequest
+        const rowNum = rowOffset + originalRequest.batchNumber
+        const row = worksheet.getRow(rowNum)
+
+        row.getCell("B").value = originalRequest.block
+        row.getCell("C").value = originalRequest.streetName
+        row.getCell("D").value = originalRequest.area
+        row.getCell("E").value = originalRequest.suspectUnit
+        row.getCell("F").value = originalRequest.cameraFocusPoint
+
+        row.getCell("G").value = record.expand.assignedUser.name
+
+        //TODO: Clarify what should date/time reference exactly. (Cells H,I)
+        const formData = record.formData
+        row.getCell("J").value = formData.isFeasible ? "Yes" : "No"
+        if (formData.isFeasible) {
+            //TODO: Rows K-N
+            worksheet.mergeCells(`O${rowNum}:P${rowNum}`)
+            row.getCell("O").value = "N/A" //TODO: Check this works
+        } else {
+            worksheet.mergeCells(`K${rowNum}:N${rowNum}`)
+            row.getCell("K").value = "N/A" //TODO: Check this works
+            //TODO: Rows O,P
+        }
         //const reasonUrl = pb.files.getUrl(record, record.reasonImage, {'token': fileToken});
+        row.commit()
     });
+    await workbook.xlsx.writeFile(`./generated_reports/Contractor Deployment Plan Batch ${batch_num}.xlsx`);
 }
 async function main() {
     const authData = await pb.admins.authWithPassword(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD)
---
+++ backend/pocketbase_scripts/pocketbase-types.ts
@@ -0,0 +1,75 @@
+/**
+* This file was @generated using pocketbase-typegen
+*/
+
+export enum Collections {
+	SurveyDetails = "surveyDetails",
+	SurveyResults = "surveyResults",
+	Users = "users",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
+
+// System fields
+export type BaseSystemFields<T = never> = {
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
+
+export type AuthSystemFields<T = never> = {
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
+
+// Record types for each collection
+
+export type SurveyDetailsRecord = {
+	batchNumber?: number
+	batchID?: number
+	block?: string
+	streetName?: string
+	area?: string
+	suspectUnit?: string
+	cameraFocusPoint?: string
+}
+
+export type SurveyResultsRecord<TformData = unknown> = {
+	surveyRequest: RecordIdString
+	assignedUser: RecordIdString
+	formData: null | TformData
+	reasonImage: string
+	additionalImage?: string
+}
+
+export type UsersRecord = {
+	name?: string
+	avatar?: string
+}
+
+// Response types include system fields and match responses from the PocketBase API
+export type SurveyDetailsResponse = Required<SurveyDetailsRecord> & BaseSystemFields
+export type SurveyResultsResponse<TformData = unknown, Texpand = unknown> = Required<SurveyResultsRecord<TformData>> & BaseSystemFields<Texpand>
+export type UsersResponse = Required<UsersRecord> & AuthSystemFields
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+	surveyDetails: SurveyDetailsRecord
+	surveyResults: SurveyResultsRecord
+	users: UsersRecord
+}
+
+export type CollectionResponses = {
+	surveyDetails: SurveyDetailsResponse
+	surveyResults: SurveyResultsResponse
+	users: UsersResponse
+}
