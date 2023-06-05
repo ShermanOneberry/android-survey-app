a192ad31c7dae823fe4dcfd39632fddd513f92f7
ShermanOneberry <107899491+ShermanOneberry@users.noreply.github.com>
2023-06-05 13:19:02 +0800

    Convert to excel4node

--- backend/pocketbase_scripts/src/generate_report.ts
+++ backend/pocketbase_scripts/src/generate_report.ts
@@ -1,7 +1,8 @@
 import PocketBase from 'pocketbase';
 import { TformData, Texpand } from "./types/pocketbase-get-types.ts"
 import {Collections, SurveyResultsResponse} from "./types/pocketbase-types.ts"
-import Excel from 'exceljs';
+import xl from "excel4node"
+import fs from "fs"
 import axios from 'axios';
 
 import dotenv from 'dotenv'
@@ -77,6 +78,115 @@ function generateLocationDescription(report: TformData): string {
             return "Deploy at roof of $generalLocationDescription" 
     }
 }
+function createExcelBaseTemplate(batch_num: number): [xl.Workbook, xl.Worksheet, xl.Style]  {
+    const workbook = new xl.Workbook();
+    const worksheet = workbook.addWorksheet(
+        "Contractor's Deployment Plans",
+        {
+            sheetView: {
+                zoomScale: 60
+            }
+        });
+
+    worksheet.cell(1,1).string("CONTRACTOR'S DEPLOYMENT PLAN")
+        .style({
+            font: {
+                size: 16,
+                bold: true,
+                name: "Times New Roman"
+            },
+        })
+    worksheet.cell(2,1).string(`BATCH NO: ${batch_num}`)
+        .style({
+            font: {
+                size: 16,
+                name: "Times New Roman"
+            },
+        })
+    const commonStyleData: xl.Style = {
+        font: {
+            size: 16,
+            name: "Times New Roman",
+        },
+        alignment: {
+            vertical: "center",
+            horizontal: "center",
+            wrapText: true
+        },
+        border: {
+            left: {
+                style: "thin"
+            },
+            right: {
+                style: "thin"
+            },
+            top: {
+                style: "thin"
+            },
+            bottom: {
+                style: "thin"
+            },
+        },
+        fill: {
+            type: "pattern",
+            patternType: "solid",
+            fgColor: "F2F2F2"
+        }
+    }
+    const cellStyle = workbook.createStyle(commonStyleData)
+    commonStyleData.font.bold = true
+    const headerStyle = workbook.createStyle(commonStyleData)
+
+    worksheet.row(5).setHeight(81.6)
+
+    worksheet.cell(5,1).string("No").style(headerStyle)
+    worksheet.column(1).setWidth(4.67)
+    worksheet.cell(5,2).string("Block").style(headerStyle)
+    worksheet.column(2).setWidth(12.89)
+    worksheet.cell(5,3).string("Street Name").style(headerStyle)
+    worksheet.column(3).setWidth(34.67)
+    worksheet.cell(5,4).string("Area").style(headerStyle)
+    worksheet.column(4).setWidth(14.89)
+    worksheet.cell(5,5).string("Suspect Unit(s)").style(headerStyle)
+    worksheet.column(5).setWidth(24.89)
+    worksheet.cell(5,6).string("Camera Focus Point").style(headerStyle)
+    worksheet.column(6).setWidth(24.33)
+
+    worksheet.cell(4,7,4,10, true).string("ASSESSMENT").style(headerStyle)
+
+    worksheet.cell(5,7).string("Team Assignment").style(headerStyle)
+    worksheet.column(7).setWidth(15.56)
+    worksheet.cell(5,8).string("Date").style(headerStyle)
+    worksheet.column(8).setWidth(13.67)
+    worksheet.cell(5,9).string("Time/hrs").style(headerStyle)
+    worksheet.column(9).setWidth(11.78)
+    worksheet.cell(5,10).string("Feasible\n(Yes/No)").style(headerStyle)
+    worksheet.column(10).setWidth(12.22)
+
+    worksheet.cell(4,11,4,14, true).string("FEASIBLE TO DEPLOY").style(headerStyle)
+
+    worksheet.cell(5,11).string("No. of Boxes").style(headerStyle)
+    worksheet.column(11).setWidth(9.33)
+    worksheet.cell(5,12).string("No. of Cameras").style(headerStyle)
+    worksheet.column(12).setWidth(13.11)
+    worksheet.cell(5,13).string("Description of Planned Deployment Location").style(headerStyle)
+    worksheet.column(13).setWidth(29.22)
+    worksheet.cell(5,14).string("Photo of Planned Deployment Location").style(headerStyle)
+    worksheet.column(14).setWidth(29.22)
+
+    worksheet.cell(4,15,4,16, true).string("NOT FEASIBLE TO DEPLOY").style(headerStyle)
+
+    worksheet.cell(5,15).string("Reason").style(headerStyle)
+    worksheet.column(15).setWidth(31.67)
+    worksheet.cell(5,16).string("Suggested Solutions/suggested locations (another option)").style(headerStyle)
+    worksheet.column(16).setWidth(27.11)
+
+    worksheet.cell(5,17).string("Technician's Note").style(headerStyle)
+    worksheet.column(17).setWidth(34.33)
+    worksheet.cell(5,18).string("Site Survey Conclusion").style(headerStyle)
+    worksheet.column(18).setWidth(16.67)
+    return [workbook, worksheet, cellStyle]
+}
 
 async function generate_batch_report(batch_num: number){
     const records = await pb.collection(Collections.SurveyResults)
@@ -84,57 +194,47 @@ async function generate_batch_report(batch_num: number){
         expand: "surveyRequest,assignedUser",
         filter: `surveyRequest.batchNumber=${batch_num}`,
     });
-    const workbook = new Excel.Workbook();
-    await workbook.xlsx.readFile("./template/Contractor Deployment Plan Batch XXX.xlsx");
-    const worksheet = workbook.getWorksheet("Contractor's Deployment Plans");
-    if (worksheet === undefined) {
-        return //TODO: Throw error
-    }
-    worksheet.getCell("A2").value = `BATCH NO: ${batch_num}`
+    const [workbook, worksheet, cellStyle] = createExcelBaseTemplate(batch_num)
     
     for (const record of records){
         console.log(record)
         const rowOffset = 5
         const originalRequest = record.expand.surveyRequest
         const rowNum = rowOffset + originalRequest.batchNumber
-        const row = worksheet.getRow(rowNum)
 
-        row.getCell("B").value = originalRequest.block
-        row.getCell("C").value = originalRequest.streetName
-        row.getCell("D").value = originalRequest.area
-        row.getCell("E").value = originalRequest.suspectUnit
-        row.getCell("F").value = originalRequest.cameraFocusPoint
+        worksheet.row(rowNum).setHeight(249.75)
 
-        row.getCell("G").value = record.expand.assignedUser.name
+        worksheet.cell(rowNum, 1).number(originalRequest.batchNumber).style(cellStyle)
+        worksheet.cell(rowNum, 2).string(originalRequest.block).style(cellStyle).style({font: {bold: true}})
+        worksheet.cell(rowNum, 3).string(originalRequest.streetName).style(cellStyle).style({font: {bold: true}})
+        worksheet.cell(rowNum, 4).string(originalRequest.area).style(cellStyle)
+        worksheet.cell(rowNum, 5).string(originalRequest.suspectUnit).style(cellStyle)
+        worksheet.cell(rowNum, 6).string(originalRequest.cameraFocusPoint).style(cellStyle)
+
+        worksheet.cell(rowNum, 7).string(record.expand.assignedUser.name).style(cellStyle)
         const formData = record.formData
 
-        row.getCell("H").value = formData.surveyDate //TODO: Make formatting match reference report
-        row.getCell("I").value = formData.surveyTime //TODO: Make formatting match reference report
-        row.getCell("J").value = formData.isFeasible ? "Yes" : "No"
+        worksheet.cell(rowNum, 8).string(formData.surveyDate).style(cellStyle) //TODO: Make formatting match reference report
+        worksheet.cell(rowNum, 9).string(formData.surveyTime).style(cellStyle) //TODO: Make formatting match reference report
+        worksheet.cell(rowNum, 10).string(formData.isFeasible ? "Yes" : "No").style(cellStyle)
 
         const reasonImageBuffer = await get_image_from_pocketbase(record, record.reasonImage)
-        const reasonImageID = workbook.addImage({ //TODO: Figure out why this breaks value assignment of cells
-            buffer: reasonImageBuffer,
-            extension: "jpeg", //Assumption of phone picture format
-        });
+
         if (formData.isFeasible) {
-            row.getCell("K").value = formData.boxCount
-            row.getCell("L").value = formData.cameraCount
-            row.getCell("M").value = generateLocationDescription(formData)
-            console.log(row.getCell("M").value)
-            worksheet.addImage(reasonImageID, `N${rowNum}:N${rowNum}`)
-            worksheet.mergeCells(`O${rowNum}:P${rowNum}`)
-            row.getCell("O").value = "N/A" //TODO: Check this works
-            console.log("TESTING TESTING 123")
+            worksheet.cell(rowNum, 11).string(formData.boxCount).style(cellStyle)
+            worksheet.cell(rowNum, 12).string(formData.cameraCount).style(cellStyle)
+            worksheet.cell(rowNum, 13).string(generateLocationDescription(formData)).style(cellStyle)
+            //TODO: Add image
+            worksheet.cell(rowNum, 15, rowNum, 16, true).string("N/A").style(cellStyle)//TODO: Check this works
         } else {
-            worksheet.mergeCells(`K${rowNum}:N${rowNum}`)
-            row.getCell("K").value = "N/A" //TODO: Check this works
-            //TODO: Rows O,P
+            worksheet.cell(rowNum,11,rowNum,14,true).string("N/A").style(cellStyle) //TODO: Check this works
+            //TODO: column 15,16
         }
         //const reasonUrl = pb.files.getUrl(record, record.reasonImage, {'token': fileToken});
-        row.commit()
+        //row.commit()
     }
-    await workbook.xlsx.writeFile(`./generated_reports/Contractor Deployment Plan Batch ${batch_num}.xlsx`);
+    const buffer = await workbook.writeToBuffer();
+    fs.writeFileSync(`./generated_reports/Contractor Deployment Plan Batch ${batch_num}.xlsx`, buffer);
 }
 async function main() {
     const authData = await pb.admins.authWithPassword(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD)
@@ -146,4 +246,3 @@ await main().catch((error) => {
         console.error('An error occurred while generating the report:', error);
         process.exit(1); 
 })
-test
